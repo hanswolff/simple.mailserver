386860797ef2b14448e760222608fa99ef93a901
Hans Wolff <hans.wolff@gmx.de>
2013-10-13 11:59:20 +0100

    added simple MailServer example project that stores received mails in files

---
+++ Simple.MailServer.Example/ExampleDataResponder.cs
@@ -0,0 +1,74 @@
+﻿using Simple.MailServer.Smtp;
+using System;
+using System.IO;
+using System.Text;
+using Simple.MailServer.Smtp.Config;
+
+namespace Simple.MailServer.Example
+{
+    class ExampleDataResponder : DefaultSmtpDataResponder
+    {
+        private readonly string _mailDir;
+
+        public ExampleDataResponder(IConfiguredSmtpRestrictions configuration, string mailDir)
+            : base(configuration)
+        {
+            _mailDir = mailDir;
+            EnsureDirExists(mailDir);
+        }
+
+        private static void EnsureDirExists(string directory)
+        {
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+        }
+
+        public override SmtpResponse DataStart(SmtpSessionInfo sessionInfo)
+        {
+            Console.WriteLine("Start receiving mail: {0}", GetFileNameFromSessionInfo(sessionInfo));
+            return SmtpResponse.DataStart;
+        }
+
+        private string GetFileNameFromSessionInfo(SmtpSessionInfo sessionInfo)
+        {
+            var fileName = Path.Combine(_mailDir, sessionInfo.CreatedTimestamp.ToString("yyyy-MM-dd_HHmmss_fff") + ".eml");
+            return fileName;
+        }
+
+        public override SmtpResponse DataLine(SmtpSessionInfo sessionInfo, byte[] lineBuf)
+        {
+            var fileName = GetFileNameFromSessionInfo(sessionInfo);
+
+            Console.WriteLine("{0} <<< {1}", fileName, Encoding.UTF8.GetString(lineBuf));
+
+            using (var stream = File.OpenWrite(fileName))
+            {
+                stream.Seek(0, SeekOrigin.End);
+                stream.Write(lineBuf, 0, lineBuf.Length);
+
+                stream.WriteByte(13);
+                stream.WriteByte(10);
+            }
+
+            return SmtpResponse.None;
+        }
+
+        public override SmtpResponse DataEnd(SmtpSessionInfo sessionInfo)
+        {
+            var fileName = GetFileNameFromSessionInfo(sessionInfo);
+            var size = GetFileSize(fileName);
+
+            Console.WriteLine("Mail received ({0} bytes): {1}", size, fileName);
+
+            var response = SmtpResponse.OK.Clone();
+            response.ResponseText = String.Format("{0} bytes received", size);
+
+            return response;
+        }
+
+        private long GetFileSize(string fileName)
+        {
+            return new FileInfo(fileName).Length;
+        }
+    }
+}
---
+++ Simple.MailServer.Example/Program.cs
@@ -0,0 +1,36 @@
+﻿using Simple.MailServer.Logging;
+using Simple.MailServer.Smtp;
+using System;
+using System.Net;
+
+namespace Simple.MailServer.Example
+{
+    class Program
+    {
+        private const string RootMailDir = @"C:\Temp\Mail\";
+
+        static void Main()
+        {
+            MailServerLogger.Set(new MailServerConsoleLogger(MailServerLogLevel.Debug));
+
+            using (StartSmtpServer())
+            {
+                Console.WriteLine("Hit ENTER to return.");
+                Console.ReadLine();
+            }
+        }
+
+        private static SmtpServer StartSmtpServer()
+        {
+            var smtpServer = new SmtpServer();
+            smtpServer.Configuration.DefaultGreeting = "Simple.MailServer Example";
+            smtpServer.DefaultResponderFactory = new SmtpResponderFactory(smtpServer.Configuration)
+            {
+                DataResponder = new ExampleDataResponder(smtpServer.Configuration, RootMailDir)
+            };
+
+            smtpServer.BindAndListenTo(IPAddress.Loopback, 25);
+            return smtpServer;
+        }
+    }
+}
